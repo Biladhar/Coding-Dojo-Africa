5f12fa5b707edf8bcffb376d2f8f31113eea7dd6
Biladhar <laadharbilel09@gmail.com>
2024-01-22 19:51:59 +0100

    getOnThatRide

--- Programming Basics/Laadhar_Bilel_JS_CreateVariables.js
+++ Programming Basics/Laadhar_Bilel_JS_CreateVariables.js
@@ -2,3 +2,26 @@ var riderAge;
 // to have a ride must be >=10 
 var riderHeight;
 // to have a ride must be >=42
+
+if (riderHeight >= 42) {
+    console.log("Get on that ride, kiddo!")
+}
+else {
+    console.log("Sorry kiddo. Maybe next year.")
+}
+
+//Stretch Feature 1
+if (riderHeight >= 42 && riderAge >= 10) {
+    console.log("Get on that ride, kiddo!")
+}
+else {
+    console.log("Sorry kiddo. Maybe next year.")
+}
+
+//Stretch Feature 2
+if (riderHeight >= 42 || riderAge >= 10) {
+    console.log("Get on that ride, kiddo!")
+}
+else {
+    console.log("Sorry kiddo. Maybe next year.")
+}
