e06ddeb510e2a6a370ea726e0ad1aa3460be968d
Biladhar <laadharbilel09@gmail.com>
2024-01-22 18:32:48 +0100

    code.js

---
+++ Programming Basics/code.js
@@ -0,0 +1,7 @@
+// this variable  serves as a counter for tracking the number of likes.
+var likeCount = 0;
+// this function will increase the number of likes .
+function increaseLikes() {
+    // this operation will increase by 1the number of count. 
+    likeCount = likeCount + 1;
+}
