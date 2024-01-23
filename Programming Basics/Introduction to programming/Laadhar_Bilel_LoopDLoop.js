c3960f060307b28d11cff853eff6731288747f01
Biladhar <laadharbilel09@gmail.com>
2024-01-23 18:53:31 +0100

    loop

--- Programming Basics/Introduction to programming/Laadhar_Bilel_LoopDLoop.js
+++ Programming Basics/Introduction to programming/Laadhar_Bilel_LoopDLoop.js
@@ -8,11 +8,11 @@
 
 
 
-for(var i=0 ; i<= 6 ; i++){
-    if (i % 2 === 0 && i != 6){
-        console.log(i + " miles nice joob , have a cookie")
-    }
-    else if (i = 6 ){
-        console.log(i + " miles , last candy")
+for(var miles=0 ; miles<= 6 ; miles++){
+    if (miles % 2 === 0){
+        if (miles != 0)
+        console.log(miles + " miles nice joob , Take a cookie")
     }
 }
+
+//-------------------------------------------------------------------
