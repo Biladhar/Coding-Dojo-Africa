e5e7ae4214dce8ec16ef05b996d3bee902778366
Biladhar <laadharbilel09@gmail.com>
2024-01-23 18:38:49 +0100

    loop-d-loop

---
+++ Programming Basics/Introduction to programming/Laadhar_Bilel_LoopDLoop.js
@@ -0,0 +1,18 @@
+//1. we need a loop because we have a reccurent task "give 1 candy every 2 miles" and an end condition " 6 miles".
+//2. the starting point of the loop is a runner reaches 2 miles
+//3. the loop should stop when a runner reaches 6 miles
+//4. the loop will know when to stop when we reaches 6 miles
+//5. for each iteration of the loop we increment by 1 miles
+//6. we nned a variable for Miles
+
+
+
+
+for(var i=0 ; i<= 6 ; i++){
+    if (i % 2 === 0 && i != 6){
+        console.log(i + " miles nice joob , have a cookie")
+    }
+    else if (i = 6 ){
+        console.log(i + " miles , last candy")
+    }
+}
