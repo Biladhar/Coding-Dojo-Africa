aefdf6a6b1d1c08dc87eec77d933f017b000bf27
Biladhar <laadharbilel09@gmail.com>
2024-01-30 13:02:37 +0100

    form

--- Programming_Basics/web_Basics_v 22_ft/Reflection_Assignment_Optional.js
+++ Programming_Basics/web_Basics_v 22_ft/Reflection_Assignment_Optional.js
@@ -6,6 +6,6 @@
 
 // yes , i knew befor the bootcamp that you learn by failure but i had a real problem with the shame and interaction about Reflection_Assignment_Optionalmy failure . but after what i read in the bootcamp and the general atmosphere here i feel better and i think am moving forward this kind of negativ behaviour.
 
-// 3.what do you consider your "Mistake of the Week," which was a valuable failure you learned from during Programming Basics?
+Reflection_Assignment_Optional// 3.what do you consider your "Mistake of the Week," which was a valuable failure you learned from during Programming Basics?
 
 // my failure of the week is that i wrote a function without to set the return .
