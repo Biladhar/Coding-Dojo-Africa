db1359f4e774471466cf8e931b7fcaaa87976ca0
Biladhar <laadharbilel09@gmail.com>
2024-02-22 18:42:05 +0100

    understandig_routing

---
+++ Python/Week 1/Day 4/Practice/understanding_routing/server.py
@@ -0,0 +1,27 @@
+from flask import Flask  # Import Flask to allow us to create our app
+
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+
+@app.route('/Dojo')
+def Dojo():
+    return 'Dojo'
+
+@app.route('/say/<string:name>')
+def say(name):
+    return "Hi " + name + "!"
+
+@app.route('/repeat/<int:number>/<string:word>')
+def repeat(number, word):
+    return f"{word*int(number)}"
+
+
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True, host='localhost')    # Run the app in debug mode.
