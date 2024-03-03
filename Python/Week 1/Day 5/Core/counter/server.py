0e585849e2c1c4b8d999ef89214c71b7850e6d94
Biladhar <laadharbilel09@gmail.com>
2024-03-03 18:32:21 +0100

    update counter

--- Python/Week 1/Day 5/Core/counter/server.py
+++ Python/Week 1/Day 5/Core/counter/server.py
@@ -1,28 +1,37 @@
-from flask import Flask, render_template, session, redirect, url_for, request
+from flask import Flask , render_template, redirect, request, session # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+app.secret_key = "hello dojo"
 
-app = Flask(__name__)
-app.secret_key = "votre_clé_secrète"
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if 'visits' not in session:
-        session['visits'] = 0
+# * view route
 
-    if request.method == 'POST':
-        action = request.form.get('action')
-        if action == 'count':
-            session['visits'] += 1
-        elif action == 'reset':
-            session['visits'] = 0
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def view_counter():
+    if "visit" not in session:
+        session["visit"]= 0
+    else:
+        session["visit"] += 1
+    return render_template("index.html")  # Return the string 'Hello World!' as a response
 
-    return render_template('index.html', visits=session['visits'])
+# ! action route
+
 
 @app.route('/destroy_session')
 def destroy_session():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect("/")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/add",methods=["post"])
+def add_1():
+    return redirect ("/")
+
+@app.route("/add2",methods=["post"])
+def add_2():
+    session["visit"] += 1
+    return redirect ("/")
 
 
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
