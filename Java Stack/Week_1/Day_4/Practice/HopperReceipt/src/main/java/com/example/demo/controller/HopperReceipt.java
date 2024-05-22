2a97f1c2f0a3834d343a5bfaa7d0c212a652def1
Biladhar <laadharbilel09@gmail.com>
2024-05-22 22:20:05 +0100

    second_test

--- Java Stack/Week_1/Day_4/Practice/HopperReceipt/src/main/java/com/example/demo/controller/HopperReceipt.java
+++ Java Stack/Week_1/Day_4/Practice/HopperReceipt/src/main/java/com/example/demo/controller/HopperReceipt.java
@@ -11,6 +11,7 @@ public class HopperReceipt {
     @RequestMapping("/")
     public String index(Model model) {
         
+        // Create attributes
         String name = "Grace Hopper";
         String itemName = "Copper wire";
         double price = 5.25;
@@ -24,7 +25,19 @@ public class HopperReceipt {
         model.addAttribute("description", description);
         model.addAttribute("vendor", vendor);
 
-    
+        //second test attributes
+        String name2 = "Dark Vador";
+        String itemName2 = "Light Saber";
+        double price2 = 1000.00;
+        String description2 = "The weapon of a Jedi Knight. Not as clumsy or random as a blaster. An elegant weapon for a more civilized age.";
+        String vendor2 = "Galactic Empire";
+
+        // Add attributes to the model
+        model.addAttribute("name2", name2);
+        model.addAttribute("itemName2", itemName2);
+        model.addAttribute("price2", price2);
+        model.addAttribute("description2", description2);
+        model.addAttribute("vendor2", vendor2);
     
         return "index.jsp";
     }
--- Java Stack/Week_1/Day_4/Practice/HopperReceipt/src/main/webapp/WEB-INF/index.jsp
+++ Java Stack/Week_1/Day_4/Practice/HopperReceipt/src/main/webapp/WEB-INF/index.jsp
@@ -9,11 +9,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-    <h1>Customer Name: <c:out value="${name}" /></h1>
-    <h3>Item name : <c:out value="${itemName}" /></h3>
-    <h3>Price : $<c:out value="${price}" /></h3>
-    <h3>Description : <c:out value="${description}"/></h3>
-    <h3>Vendor : <c:out value="${vendor}"/></h3>
+    <h1>Customer Name: <c:out value="${name2}" /></h1>
+    <h3>Item name : <c:out value="${itemName2}" /></h3>
+    <h3>Price : $<c:out value="${price2}" /></h3>
+    <h3>Description : <c:out value="${description2}"/></h3>
+    <h3>Vendor : <c:out value="${vendor2}"/></h3>
 
 </body>
 </html>
